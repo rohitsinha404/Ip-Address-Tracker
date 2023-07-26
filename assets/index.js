e7c239c5bc4da8080b995a469cb6435a09f006e1
Rohit Sinha <114895566+rohitsinha404@users.noreply.github.com>
2023-07-26 11:22:31 +0530

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,5 @@
-
-let jsonData;
-let ipAdd = "8.8.8.8";
-let lat = 28.61;
-let lon = 77.20;
+// Variable declarations
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmZkMjM0MzQiLCJhIjoiY2xqbWc2ZmpiMHBuNjNscjdtN2Z3Z3NidCJ9.YwMCLIXng5gUDgC9LiAoEg';
 
 const ip = document.querySelector("#ipBox");
 const loc = document.querySelector("#locBox");
@@ -11,30 +8,48 @@ const isp = document.querySelector("#ispBox");
 
 const input = document.getElementById("myInput");
 
-input.addEventListener("keypress", function (event) {
+// Event listeners
+document.getElementById("myInput").addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
         event.preventDefault();
         document.getElementById("btn").click();
     }
 });
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZmZkMjM0MzQiLCJhIjoiY2xqbWc2ZmpiMHBuNjNscjdtN2Z3Z3NidCJ9.YwMCLIXng5gUDgC9LiAoEg';
+document.getElementById("btn").addEventListener("click", async function () {
+    const input = document.getElementById("myInput").value;
+    await fetchIPData(input);
+});
 
+// Map initialization
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [lon, lat], // default starting position [lng, lat]
+    center: [77.20, 28.61], // default starting position [lng, lat]
     zoom: 12, // default starting zoom
 });
 
-function handleEnterKeyPress(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        document.getElementById("btn").click();
+// Fetch IP data
+async function fetchIPData(ipAddress) {
+    try {
+        const response = await fetch(`https://ipapi.co/${ipAddress}/json/`);
+        if (!response.ok) {
+            throw new Error('Network response was not OK');
+        }
+        const jsonData = await response.json();
+        const { latitude, longitude, city, country_name, timezone, utc_offset, org } = jsonData;
+
+        ip.innerText = ipAddress;
+        loc.innerText = `${city} ${country_name}`;
+        timezone.innerText = `${timezone} ${utc_offset}`;
+        isp.innerText = org;
+
+        // Update the map's center coordinates
+        map.setCenter([longitude, latitude]);
+    } catch (error) {
+        console.log('An error occurred:', error);
     }
 }
-
-input.addEventListener("keypress", handleEnterKeyPress);
 
 fetch('https://ipapi.co/json/')
     .then(function (response) {
@@ -51,36 +66,3 @@ fetch('https://ipapi.co/json/')
     .catch(function (error) {
         console.log(error);
     });
-
-function getIp() {
-    const input = document.getElementById("myInput").value;
-    ipAdd = input;
-    console.log(input);
-    fetchIPData();
-}
-
-function fetchIPData() {
-    fetch(`https://ipapi.co/${ipAdd}/json/`)
-        .then(function (response) {
-            if (!response.ok) {
-                throw new Error('Network response was not OK');
-            }
-            return response.json();
-        })
-        .then(function (data) {
-            jsonData = data;
-            lat = jsonData.latitude;
-            lon = jsonData.longitude;
-            ip.innerText = ipAdd;
-            loc.innerText = `${jsonData.city} ${jsonData.country_name}`;
-            timezone.innerText = `${jsonData.timezone} ${jsonData.utc_offset}`;
-            isp.innerText = jsonData.org;
-
-            // Update the map's center coordinates
-            map.setCenter([lon, lat]);
-        })
-        .catch(function (error) {
-            console.log('An error occurred:', error);
-        });
-}
-
